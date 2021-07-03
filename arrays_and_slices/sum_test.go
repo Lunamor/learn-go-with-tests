75d7a657109c77002acb7fef1ead622a5a5a9573
Lunamor <hongyi.meng@jigjog.org>
2021-07-02 22:18:20 -0400

    LC-7 #comment Sum using array of size 5

--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -3,11 +3,25 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-    nums := [5]int{1, 2, 3, 4, 5}
-    got := Sum(nums)
-    want := 15
 
-    if got != want {
-        t.Errorf("want %d given %v, but got %q", want, nums, got)
-    }
+    t.Run("collection of 5 numbers", func(t Testing.T) {
+        nums := [5]int{1, 2, 3, 4, 5}
+        got := Sum(nums)
+        want := 15
+
+        if got != want {
+            t.Errorf("want %d given %v, but got %q", want, nums, got)
+        }
+    })
+
+    t.Run("collection of any size", func(t *testing.T) {
+        nums := []int{1, 2, 3}    
+        
+        got := Sum(nums)
+        want := 6
+
+       if got != want {
+            t.Errorf("want %d given %v, but got %q", want, nums, got)
+        }
+    })
 }
