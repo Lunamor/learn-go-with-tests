76f78f498bf33d14b894ed20b53f064e3d79cfd5
Lunamor <hongyi.meng@jigjog.org>
2021-07-02 22:46:56 -0400

    LC-7 #comment using make()

---
+++ arrays_and_slices/sum.go
@@ -0,0 +1,19 @@
+package main
+
+func Sum(nums []int) int {
+    sum := 0
+    for _, num := range nums {
+        sum += num
+    }
+    return sum
+}
+
+func SumAll(numsToSum ...[]int) []int {
+    lengthOfNumbers := len(numsToSum)
+    sums := make([]int, lengthOfNumbers)
+
+    for i, nums := range numsToSum {
+        sums[i] = Sum(nums)
+    }
+    return sums
+}
--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import "testing"
+import "reflect"
 
 func TestSum(t *testing.T) {
 
@@ -9,9 +10,17 @@ func TestSum(t *testing.T) {
         
         got := Sum(nums)
         want := 6
-
        if got != want {
             t.Errorf("want %d given %v, but got %q", want, nums, got)
         }
     })
+}
+
+func TestSumAll(t *testing.T) {
+    got := SumAll([]int{1, 2}, []int{0, 9})
+    want := []int{3, 9}
+
+    if !reflect.DeepEqual(want, got) {
+        t.Errorf("want %v but got %v", want, got)
+    }
 }
