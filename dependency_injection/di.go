885247d9d75bd9a487dc9f2aad9fb1de69b29114
Lunamor <hongyi.meng@jigjog.org>
2021-07-03 20:58:19 -0500

    LC-11 #comment inside Printf

---
+++ dependency_injection/di.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+    "io"
+    "os"
+    "log"
+    "net/http"
+)
+
+
+func Greet(writer io.Writer, name string) {
+    fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+    Greet(w, "world")
+}
+
+func main() {
+    //prints
+    Greet(os.Stdout, "Elodie")
+    //write to the internet
+    log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler)))
+}
