517bbfeb70901e8d9a2b4d320cd5002b167225eb
Lunamor <hongyi.meng@jigjog.org>
2021-07-03 23:37:20 -0400

    LC-11 #comment test for sleep/write order and replaced old spy

--- mocking/count.go
+++ mocking/count.go
@@ -7,8 +7,24 @@ import (
     "time"
 )
 
+const write = "write"
+const sleep = "sleep"
+
 type Sleeper interface {
     Sleep()
+}
+
+type CountdownOperationsSpy struct {
+    Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+    s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+    s.Calls = append(s.Calls, write)
+    return
 }
 
 type DefaultSleeper struct {}
